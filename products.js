9b3e9edb1773c678af8455741b9c5d931d620563
Reason <reasonpokhrel123@gmail.com>
2025-01-13 13:55:50 +0545

    Add files via upload

---
+++ products.js
@@ -0,0 +1,9 @@
+export let products = [
+  {
+    product_id : "1",
+    product_name : "Omega Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/watches/Home_SP_Pilot_KV_Push.avif"
+  }
+];
