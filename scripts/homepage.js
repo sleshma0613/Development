1d0f1474fd4d84545e924e001c16d2b8d459fc83
Reason186 <reasonpokhrel123@gmail.com>
2025-01-17 01:37:43 +0545

    uffffff

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,30 +1,68 @@
 let products = [
   {
     product_id : "1",
-    product_name : "Omega Pilot",
+    product_name : "Omega Speedmaster Pilot",
     product_price : "300000 NPR",
     product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
     product_image: "../images/watches/Home_SP_Pilot_KV_Push.avif"
+  },
+  {
+    product_id : "1",
+    product_name : "Omega Speedmaster Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/watches/advisor-vintage.jpg"
+  },
+  {
+    product_id : "1",
+    product_name : "Omega Speedmaster Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/watches/movado-vintage.jpg"
+  },
+  {
+    product_id : "1",
+    product_name : "Omega Speedmaster Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/watches/rolex-bubble-back.jpg"
+  },
+  {
+    product_id : "1",
+    product_name : "Omega Speedmaster Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/watches/rolexDatejust.jpg"
+  },
+  {
+    product_id : "1",
+    product_name : "Omega Speedmaster Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/watches/Zenith-El-Primero.jpg"
+  },
+  {
+    product_id : "1",
+    product_name : "Omega Speedmaster Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/W1.jpg"
+  },
+  {
+    product_id : "1",
+    product_name : "Omega Speedmaster Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/W3.jpg"
+  },
+  {
+    product_id : "1",
+    product_name : "Omega Speedmaster Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/W7.jpg"
   }
 ];
-
-// let slideImageElement = document.querySelector(".slides-js");
-
-// function slideshow(num) {
-//   slideImageElement.innerHTML = `
-//     <img src="images/slide${num}.jpg" width = 100% height = 440px style = "
-//     object-fit: cover
-//     ">
-//   `
-// }
-
-// function runSlideshow() {
-//   for (i = 2; i <= 3; i++) {
-//     slideshow(i);
-//   }
-// }
-
-// runSlideshow();
 
 function renderFeaturedProducts() {
   const productElement = document.querySelector(".featured-catalogue-js");
@@ -32,26 +70,7 @@ function renderFeaturedProducts() {
     productElement.innerHTML += `
     <div class="featured-product">
       <div class="fproduct-image-container">
-        <img class="product-image" src=${products[0].product_image}>
-      </div>
-      <div class="fproduct-info">
-        <p class="product-name">
-        ${products[i].product_name}
-        </p>
-        <p class="product-price">
-        ${products[i].product_price}
-        </p>
-        <p class="product-description">
-        ${products[i].product_description}
-        </p>
-        <div class="fproduct-options">
-          <button class="buy-button action-button">
-            BUY NOW
-          </button>
-          <button class="add-to-cart action-button">
-            ADD TO CART
-          </button>
-        </div>
+        <img class="product-image" src=${products[i].product_image}>
       </div>
     </div>
   `
@@ -60,13 +79,33 @@ function renderFeaturedProducts() {
 
 renderFeaturedProducts();
 
-function renderCategoriesSection() {
-  const categoriesElement = document.querySelector(".categories-js");
-  categoriesElement.innerHTML = `
-    <div class="category-heading">
-    CATEGORIES
-    </div>
-  `
-}
+let slideImageElement = document.querySelector(".slide-image");
+let imageIndex = 1;
 
-renderCategoriesSection();
+function nextSlide() {
+  if (imageIndex < 3) {
+    imageIndex++;
+    slideImageElement.src = `images/slide${imageIndex}.jpg`;
+  } else {
+    imageIndex = 1;
+    slideImageElement.src = `images/slide${imageIndex}.jpg`;
+  }
+};
+
+function prevSlide() {
+  if (imageIndex > 1) {
+    imageIndex--;
+    slideImageElement.src = `images/slide${imageIndex}.jpg`;
+  } else {
+    imageIndex = 3;
+    slideImageElement.src = `images/slide${imageIndex}.jpg`;
+  }
+};
+
+document.querySelector(".next-slide").addEventListener('click', () => {
+  nextSlide();
+})
+
+document.querySelector(".prev-slide").addEventListener('click', () => {
+  prevSlide();
+})
