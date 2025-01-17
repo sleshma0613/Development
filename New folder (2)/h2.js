f5da07e6d5922226146b844295d4f2275aa14104
Adwaita joshi <adwaitajosi0@gmail.com>
2025-01-17 23:45:49 +0545

    .

---
+++ New folder (2)/h2.js
@@ -0,0 +1,36 @@
+let products = [
+  {
+    product_id : "1",
+    product_name : "Omega Speedmaster Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/watches/Home_SP_Pilot_KV_Push.avif"
+  },
+  {
+    product_id : "1",
+    product_name : "Omega Speedmaster Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/watches/patek-philippe-calatrava.jpg"
+  },
+  {
+    product_id : "1",
+    product_name : "Omega Speedmaster Pilot",
+    product_price : "300000 NPR",
+    product_description : "Ekdam Dami xa Ghadi, Khatraaaaa xa",
+    product_image: "../images/W1.jpg"
+  }
+];
+
+function renderFeaturedProducts() {
+  const productElement = document.querySelector(".featured-catalogue-js");
+  for (let i = 0; i <= products.length - 1; i++) {
+    productElement.innerHTML += `
+    <div class="featured-product">
+      <img class="product-image" src=${products[i].product_image}>
+    </div>
+  `
+  }
+}
+
+renderFeaturedProducts();
